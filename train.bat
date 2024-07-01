b9cec23436be11da9b12ca6df8f0c8959991c2b3
Rizky Barik <rizkyebarik@gmail.com>
2024-07-01 23:01:58 +0700

    Initial Commit

---
+++ train.bat
@@ -0,0 +1,1 @@
+py yolov5/train.py --data yolov5/data/blood_cell.yaml --batch-size 4 --epochs 500 --img-size 640 --project runs/train --name blood_cell --weights yolov5/weights/yolov5s.pt --device cpu
